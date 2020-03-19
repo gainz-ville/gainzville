278583d3568c4611a94d2f9ef7d9f901b507b5cd
Peter Gray <peter.gray95@gmail.com>
2020-03-19 16:55:11 +0000

    Fixed CORS!

--- run.gainzville.ps1
+++ run.gainzville.ps1
@@ -2,6 +2,8 @@
 # docker build . --file Dockerfile --tag gainzville:1
 
 docker run --name gainzville -p 80:80 -d gainzville:1
+docker run -p 80:80 -d gainzville:1
 
 # docker build . --file DockerfileServer --tag gainzville-server:1
 docker run --name gainzville-server -p 5050:80 -d gainzville-server:1
+docker run -p 5050:80 -d gainzville-server:1
