33ac537f8e2a1f5d971945e11231b82f6c521c73
Peter Gray <peter.gray95@gmail.com>
2020-04-10 15:23:42 +0100

    added database auto migration on startup

--- Gainzville.Server/Startup.cs
+++ Gainzville.Server/Startup.cs
@@ -63,9 +63,14 @@ namespace Gainzville.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            GainzDbContext context)
         {
             app.UseResponseCompression();
+
+            context.Database.Migrate();
 
             if (env.IsDevelopment())
             {
